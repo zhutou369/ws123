4cced304d6e8e25afab28e18d0e8c4c10b857976
zhutou757 <zhutou757@gmail.com>
2026-05-12 17:18:24 +0800

    feat: add watch target for posts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,13 @@
 module.exports = function(eleventyConfig) {
-  // 告诉 11ty：把 admin 文件夹和里面的内容直接拷贝到输出目录
+  // 1. 核心：监视 posts 文件夹。哪怕是文章内容变了，11ty 也会重新构建
+  eleventyConfig.addWatchTarget("./posts/");
+
+  // 2. 静态资源搬运：确保 admin 和 assets 文件夹被直接拷贝到生成的网站中
   eleventyConfig.addPassthroughCopy("admin");
-  // 如果你有图片文件夹也可以加在这里
   eleventyConfig.addPassthroughCopy("assets");
+  
+  // 3. 如果你的图片存在 posts 文件夹里，也需要拷贝它
+  // eleventyConfig.addPassthroughCopy("posts/*.jpg"); 
 
   return {
     dir: {
